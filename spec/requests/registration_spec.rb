6f84125730231420d56ed917128a2a1f47a5622d
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-01-18 03:01:08 -0700

    Add sad path testing for users post action

--- spec/requests/registration_spec.rb
+++ spec/requests/registration_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Register API' do
+  
+  #happy path
   it 'creates a user and gives them an api key' do
     user_params = {
       email: 'test@example.com',
@@ -17,5 +19,39 @@ RSpec.describe 'Register API' do
     expect(new_user[:data][:attributes].keys).to include(:email, :api_key)
     expect(new_user[:data][:type]).to eq('users')
     expect(new_user[:data][:attributes][:email]).to eq(user_params[:email])
+  end
+
+  #sad path
+  it 'returns 400 error if passwords do not match' do
+    user_params = {
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'fakepassword'
+    }
+
+    headers = { CONTENT_TYPE: 'application/json', Accept: 'application/json' }
+    post '/api/v1/users', headers: headers, params: user_params.to_json
+    error = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(error[:errors]).to eq("Error creating user")
+  end
+
+  it 'returns 400 error if email already exists' do
+    user = User.create!(email: 'test@example.com', password: 'password', password_confirmation: 'password', api_key: '123')
+    user_params = {
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+
+    headers = { CONTENT_TYPE: 'application/json', Accept: 'application/json' }
+    post '/api/v1/users', headers: headers, params: user_params.to_json
+    error = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(error[:errors]).to eq("Error creating user")
   end
 end
