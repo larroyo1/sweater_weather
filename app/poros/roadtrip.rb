34be2e91d2407cd3b2578c644dbfab180f5942ab
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-01-18 07:25:09 -0700

    add attr_reader

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -1,9 +1,11 @@
 class Roadtrip
+  attr_reader :id, :time_to_travel, :from, :to
 
   def initialize(data, from, to)
     @time_to_travel = data[:route][:formattedTime]
     @from = from
     @to = to
+    @id = nil
   end
 
   def formatted_travel_time
